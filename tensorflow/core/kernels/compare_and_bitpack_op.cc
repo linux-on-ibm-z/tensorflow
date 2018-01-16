6d9c345a3d215850e0997fdcfa32dd96757a4098
namrata-ibm <bhavenamrata@gmail.com>
2018-01-16 03:31:32 -0500

    Compare_and_bitpack function for bool for big endian

--- tensorflow/core/kernels/compare_and_bitpack_op.cc
+++ tensorflow/core/kernels/compare_and_bitpack_op.cc
@@ -111,6 +111,21 @@ struct ComputeShard<T,
       typename TTypes<uint8>::Matrix output, bool /*thresh*/, int64 start,
       int64 limit) {
     // NOTE(ebrevdo): This assumes memory is little-endian.
+    // Added support for big endian.
+#if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+    for (int64 i = start; i < limit; ++i) {
+      uint8* out = output.data() + i;
+      const int64 block = *reinterpret_cast<const int64*>(input.data() + 8 * i);
+      *out =
+          ((((block & (1LL << (7 * 8))) >> (7 * 7))) |
+           (((block & (1LL << (6 * 8))) >> (6 * 7))) |
+           (((block & (1LL << (5 * 8))) >> (5 * 7))) |
+           (((block & (1LL << (4 * 8))) >> (4 * 7))) |
+           (((block & (1LL << (3 * 8))) >> (3 * 7))) |
+           (((block & (1LL << (2 * 8))) >> (2 * 7))) |
+           (((block & (1LL << 8)) >> (1 * 7))) | (((block & (1LL)))));
+    }
+#else    
     for (int64 i = start; i < limit; ++i) {
       uint8* out = output.data() + i;
       const int64 block = *reinterpret_cast<const int64*>(input.data() + 8 * i);
@@ -123,6 +138,7 @@ struct ComputeShard<T,
            (((block & (1LL << (2 * 8))) >> (2 * 8 - 5))) |
            (((block & (1LL << 8)) >> (1 * 8 - 6))) | (((block & (1LL)) << 7)));
     }
+#endif
   }
 };
 
